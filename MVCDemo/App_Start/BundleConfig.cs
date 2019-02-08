aafc9e225a86d60f1aee7d409ea1d5569e43b9b9
Sagar <sagarsamal92@gmail.com>
2019-02-08 21:35:09 +0530

    Added proj

---
+++ MVCDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCDemo.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                                                        "~/Content/CSS/bootstrap.min.css",
+                                                        "~/Content/CSS/prettyphoto.css", "~/Content/CSS/font-awesome.min.css", "~/Content/CSS/font-awesome.min.css", "~/Content/CSS/style.css"
+                                                    ));
+            // add ScriptBundle here..  
+
+        }
+    }
+}
---
+++ MVCDemo/Models/Registration/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCDemo.Models.Registration
+{
+    public class Customer
+    {
+
+        public int CustomerID { get; set; }
+      
+        [Required(ErrorMessage ="Name required")]
+        public String CustomerName { get; set; }
+
+        public string CustomerAddressLine1 { get; set; }
+        public string CustomerAddressLine2 { get; set; }
+        public string CustomerAddressLine3 { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+    }
+}
